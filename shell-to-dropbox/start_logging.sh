c18bbb91b9c567fb54cd6abc0d367d7f83301631
James Mickley <mickley@gmail.com>
2017-01-13 08:38:49 -0500

    added documentation to start logging

--- shell-to-dropbox/start_logging.sh
+++ shell-to-dropbox/start_logging.sh
@@ -2,6 +2,9 @@
 
 LogDir=~/Application\ Libraries/Dropbox/SWC
 
+# The script utility records all terminal activity.  
+# -q = quiet mode, -t 5 flushes to disk every 5 seconds
 script -q -t 5 "$LogDir/shell.txt"
 
-source .bash_profile
+# Needs to be done manually
+#source .bash_profile
